dd634b6a0312348280409f982e5dc8ba46a04657
adil-a <adil.asif2000@hotmail.com>
2025-10-31 15:35:36 -0700

    eof fix
    Signed-off-by: adil-a <adil.asif2000@hotmail.com>

--- nemo_rl/models/policy/dtensor_policy_worker_v2.py
+++ nemo_rl/models/policy/dtensor_policy_worker_v2.py
@@ -2110,4 +2110,3 @@ def _infer_checkpoint_root(weights_path: str) -> str:
     if weights_dir.endswith("weights"):
         return os.path.dirname(weights_dir)
     return weights_dir
-                                           

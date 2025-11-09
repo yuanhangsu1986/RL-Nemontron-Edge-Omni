2fa8305b82337ca7ab7c5bc5128432fbe2de53df
Manasa Manohara <mmanohara@nvidia.com>
2025-11-08 20:19:59 -0800

    Resolving comments
    Signed-off-by: Yuki Huang <yukih@nvidia.com>
    Signed-off-by: ruit <ruit@nvidia.com>

--- nemo_rl/data/datasets/response_datasets/tulu3.py
+++ nemo_rl/data/datasets/response_datasets/tulu3.py
@@ -19,65 +19,6 @@ from datasets import load_dataset
 
 from nemo_rl.data.interfaces import TaskDataSpec
 
-
-def to_preference_data_format(
-    data: dict[str, Any],
-) -> dict[
-    str, list[dict[str, int | list[dict[str, str | Any]]]] | list[dict[str, str]]
-]:
-    chosen_conversation = data["chosen"]
-    rejected_conversation = data["rejected"]
-
-    context = chosen_conversation[:-1]
-
-    # We assume that except last assistant response, all messages in
-    # chosen and rejected conversations are similar. Validating this...
-    assert json.dumps(context, ensure_ascii=False) == json.dumps(
-        rejected_conversation[:-1], ensure_ascii=False
-    ), (
-        f"Context mismatch.\n\nchosen: {chosen_conversation}\n\n rejected: {rejected_conversation}"
-    )
-
-    # We assume that last response is always from the assistant. Validating this...
-    assert chosen_conversation[-1]["role"] == "assistant", (
-        f"The last chosen response ({chosen_conversation[-1]}) is not from assistant!"
-    )
-    assert rejected_conversation[-1]["role"] == "assistant", (
-        f"The last rejected response ({rejected_conversation[-1]}) is not from assistant!"
-    )
-
-    chosen_response = chosen_conversation[-1]["content"]
-    rejected_response = rejected_conversation[-1]["content"]
-
-    return {
-        "context": context,
-        "completions": [
-            {
-                "rank": 0,
-                "completion": [{"role": "assistant", "content": chosen_response}],
-            },
-            {
-                "rank": 1,
-                "completion": [{"role": "assistant", "content": rejected_response}],
-            },
-        ],
-    }
-
-
-class Tulu3PreferenceDataset:
-    """Tulu3 preference dataset for DPO training."""
-
-    def __init__(self) -> None:
-        ds = load_dataset(
-            path="allenai/llama-3.1-tulu-3-8b-preference-mixture",
-            trust_remote_code=True,
-        )
-        self.formatted_ds = ds.map(to_preference_data_format)
-
-        self.task_spec = TaskDataSpec(
-            task_name="Tulu3Preference",
-        )
-
 def format_tulu3_sft_mixture(data: dict[str, Any]) -> dict[str, str | dict[str, str]]:
     """format for Tulu3 SFT data."""
     messages = data["messages"]
